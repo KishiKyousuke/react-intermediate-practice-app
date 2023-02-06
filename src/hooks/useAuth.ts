9079111dab67f983b7e1ed165eec0993b24658d0
KishiKyousuke（Kyo18） <59789739+KishiKyousuke@users.noreply.github.com>
2023-02-06 21:24:42 +0900

    lec.77

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,13 @@ import axios from "axios";
 import { User } from "../types/api/user";
 import { useHistory } from "react-router-dom";
 
+import { useMessage } from "./useMessage";
+
 export const useAuth = () => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const { showMessage } = useMessage();
+
   const login = useCallback(
     (id: string) => {
       setLoading(true);
@@ -13,19 +17,20 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
         .catch(() => {
-          alert("ログインできません");
+          showMessage({ title: "ログインできません", status: "error" });
         })
         .finally(() => {
           setLoading(false);
         });
     },
-    [history]
+    [history, showMessage]
   );
 
   return { login, loading };

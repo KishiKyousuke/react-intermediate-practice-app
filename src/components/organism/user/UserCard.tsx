1d3a9804f12e2d17f50d8131b64beb929f2b2fa2
KishiKyousuke（Kyo18） <59789739+KishiKyousuke@users.noreply.github.com>
2023-02-06 22:24:22 +0900

    lec.79

--- src/components/organism/user/UserCard.tsx
+++ src/components/organism/user/UserCard.tsx
@@ -10,34 +10,30 @@ type Props = {
 export const UserCard: FC<Props> = memo((props) => {
   const { imageUrl, userName, fullName } = props;
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              boxSize="160px"
-              borderRadius="full"
-              src={imageUrl}
-              alt={userName}
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              {userName}
-            </Text>
-            <Text fontSize="sm" color="gray">
-              {fullName}
-            </Text>
-          </Stack>
-        </Box>
-      </WrapItem>
-    </Wrap>
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          boxSize="160px"
+          borderRadius="full"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
   );
 });
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,32 @@
-import { memo, FC } from "react";
+import { memo, FC, useEffect } from "react";
 
 import { UserCard } from "../organism/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers, [getUsers]);
   return (
-    <UserCard
-      imageUrl="https://source.unsplash.com/random"
-      userName="Kyo18"
-      fullName="Kyosuke Kishi"
-    />
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} mx="auto">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
